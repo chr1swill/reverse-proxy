828ccabc85e78e60448c3c7f91a5ee613c343a92
chr1swill <alexanderwilliams20003@gmail.com>
2024-12-19 19:08:00 -0500

    refactor: clean up logic

--- main.go
+++ main.go
@@ -1,123 +1,93 @@
 package main
 
-//import (
-//	"context"
-//	"fmt"
-//	"log"
-//	"net/http"
-//)
-//
-//const PORT = 8080
-//
-//func main() {
-//	mux := http.NewServeMux()
-//
-//	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-//		log.Printf("request host: %s\n", r.Host)
-//		log.Printf("request URI: %s\n", r.RequestURI)
-//
-//		var host string
-//		// modify this so it can be some sort of config or command line args
-//		switch r.Host {
-//		case "localhost:8080":
-//			{
-//				host = "0.0.0.0:42069"
-//			}
-//		default:
-//			{
-//				_ = host
-//				log.Fatalf("we got host a host we have no clue what to do with: %s\n", r.Host)
-//			}
-//		}
-//
-//		req, err := http.NewRequestWithContext(context.Background(), r.Method, fmt.Sprintf("http://%s%s", host, r.RequestURI), r.Body)
-//		if err != nil {
-//			log.Fatalf("Error in creating new request: %s\n", err)
-//		}
-//
-//		client := &http.Client{}
-//		res, err := client.Do(req)
-//		if err != nil {
-//			log.Fatalf("Error in issueing new request to client: %s\n", err)
-//		}
-//		if err := res.Write(w); err != nil {
-//			log.Fatalf("Failed to write response from client to http response writer: %s\n", err)
-//		} else {
-//			log.Println("successfully wrote response to client")
-//		}
-//	})
-//
-//	log.Printf("Server listening on port %d\n", PORT)
-//	if err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux); err != nil {
-//		log.Fatalf("Error listening and serving on port %d: %s\n", PORT, err)
-//	}
-//}
-
 import (
-  "crypto/tls"
-  "log"
-  "net/http"
-  "net/http/httputil"
-  "net/url"
+	"crypto/tls"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func newReverseProxy(target string) (*httputil.ReverseProxy, error) {
-  url, err := url.Parse(target)
-  if err != nil {
-    return nil, err
-  }
-  //proxy := httputil.NewSingleHostReverseProxy(url)
-  //proxy.ModifyResponse = func(resp *http.Response) error {return nil } // if needed
-  return httputil.NewSingleHostReverseProxy(url), nil
+	url, err := url.Parse(target)
+	if err != nil {
+		return nil, err
+	}
+	// proxy := httputil.NewSingleHostReverseProxy(url)
+	// proxy.ModifyResponse = func(resp *http.Response) error {return nil } // if needed
+	return httputil.NewSingleHostReverseProxy(url), nil
+}
+
+type targetConfig struct {
+	Host      string
+	TargetUrl string
+	CertFile  string
+	KeyFile   string
+}
+
+func newTarget(tc *targetConfig) (*httputil.ReverseProxy, tls.Certificate, error) {
+	reverseProxy, err := newReverseProxy(tc.TargetUrl)
+	if err != nil {
+		return nil, tls.Certificate{}, err
+	}
+
+	// certAristo, err := tls.LoadX509KeyPair("path/to/aristokicks/cert.pem", "path/to/aristokicks/key.pem")
+	cert, err := tls.LoadX509KeyPair(tc.CertFile, tc.KeyFile)
+	if err != nil {
+		log.Fatalf("Error loading certificate for aristokicks.ca: %s\n", err)
+		return nil, tls.Certificate{}, err
+	}
+	return reverseProxy, cert, nil
 }
 
 func main() {
-  aristoProxy, err := newReverseProxy("http://localhost:8081")
-  if err != nil {
-    log.Fatalf("Failed to create new reverse proxy for aristokicks.ca: %s\n", err)
-    return
-  }
+	arisokicksTC := &targetConfig{
+		Host:      "aristokicks.ca",
+		TargetUrl: "http://localhost:8081",
+		CertFile:  "path/to/aristokicks/cert.pem",
+		KeyFile:   "path/to/aristokicks/key.pem",
+	}
+	aristokicksProxy, aristokicksCert, err := newTarget(arisokicksTC)
+	if err != nil {
+		log.Fatalf("Failed to create new reverse proxy target for host %s: %s\n", arisokicksTC.Host, err)
+		return
+	}
 
-  locciProxy, err := newReverseProxy("http://localhost:8082")
-  if err != nil {
-    log.Fatalf("Failed to create new reverse proxy for aristokicks.ca: %s\n", err)
-    return
-  }
+	locciTC := &targetConfig{
+		Host:      "locci.ca",
+		TargetUrl: "http://localhost:8082",
+		CertFile:  "path/to/locci/cert.pem",
+		KeyFile:   "path/to/locci/key.pem",
+	}
 
-  server := &http.Server{
-    Addr: ":443",
-    Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-      switch r.Host {
-      case "aristokicks.ca":
-        aristoProxy.ServeHTTP(w, r)
-      case "locci.ca":
-        locciProxy.ServeHTTP(w, r)
-      default:
-        http.Error(w, "Not Found", http.StatusNotFound)
-      }
-    }),
-  }
+	locciProxy, locciCert, err := newTarget(locciTC)
+	if err != nil {
+		log.Fatalf("Failed to create new reverse proxy target for host %s: %s\n", arisokicksTC.Host, err)
+		return
+	}
 
-  certAristo, err := tls.LoadX509KeyPair("path/to/aristokicks/cert.pem", "path/to/aristokicks/key.pem")
-  if err != nil {
-    log.Fatalf("Error loading certificate for aristokicks.ca: %s\n", err)
-    return
-  }
+	server := &http.Server{
+		Addr: ":443",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch r.Host {
+			case arisokicksTC.Host:
+				aristokicksProxy.ServeHTTP(w, r)
+			case locciTC.Host:
+				locciProxy.ServeHTTP(w, r)
+			default:
+				http.Error(w, "Not Found", http.StatusNotFound)
+			}
+		}),
+	}
 
-  certLocci, err := tls.LoadX509KeyPair("path/to/locci/cert.pem", "path/to/locci/key.pem")
-  if err != nil {
-    log.Fatalf("Error loading certificate for locci.ca: %s\n", err)
-    return
-  }
+	server.TLSConfig = &tls.Config{
+		Certificates: []tls.Certificate{aristokicksCert, locciCert},
+		MinVersion:   tls.VersionTLS13,
+	}
 
-  server.TLSConfig = &tls.Config{
-    Certificates: []tls.Certificate{certAristo, certLocci},
-    MinVersion: tls.VersionTLS13,
-  }
-
-  log.Println("Reverse Proxy listening for connections on :443")
-  if err = server.ListenAndServeTLS("", ""); err != nil {
-    log.Fatalf("Error listening: %s\n", err)
-    return
-  }
+	log.Println("Reverse Proxy listening for connections on :443")
+	if err = server.ListenAndServeTLS("", ""); err != nil {
+		log.Fatalf("Error listening: %s\n", err)
+		return
+	}
 }
